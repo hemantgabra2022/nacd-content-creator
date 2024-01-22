57bfdb03bba2ef3d0d47490f517f5d4742f08bf7
hemantgabra2022 <hemant.g@royalcyber.com>
2024-01-22 20:18:51 +0530

    left panel and update logo

--- src/components/leftmenu.js
+++ src/components/leftmenu.js
@@ -1,5 +1,7 @@
 // src/LeftMenu.js
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/img/nacd_logo.png";
 
 const LeftMenu = () => {
   const [showSubMenu, setShowSubMenu] = useState(false);
@@ -8,6 +10,7 @@ const LeftMenu = () => {
   const toggleSubMenu = () => {
     setShowSubMenu(!showSubMenu);
   };
+
   const toggleSecondSubMenu = () => {
     setShowSecondSubMenu(!showSecondSubMenu);
   };
@@ -15,75 +18,74 @@ const LeftMenu = () => {
   return (
     <div className="bg-gray-800 text-white w-64 min-h-screen">
       <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Left Menu</h1>
+        <div className="flex flex-col items-center">
+          <img src={Logo} alt="NACD" width="70%" className="mb-4" />
+        </div>
         <ul className="space-y-2">
           <li>
-            <a
-              href="#"
+            <div
               onClick={toggleSubMenu}
-              className="flex items-center justify-between hover:bg-gray-700 px-4 py-2 rounded transition duration-300"
+              className="flex items-center justify-between hover:bg-gray-700 px-4 py-2 rounded transition duration-300 cursor-pointer"
             >
               <span>Units</span>
               <span className="ml-2">{showSubMenu ? "▲" : "▼"}</span>
-            </a>
+            </div>
             {showSubMenu && (
               <ul className="pl-6">
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="#"
                     className="block hover:bg-gray-700 px-2 py-1 rounded transition duration-300"
                   >
                     Unit & Lessons
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="#"
                     className="block hover:bg-gray-700 px-2 py-1 rounded transition duration-300"
                   >
                     Outcomes
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="#"
                     className="block hover:bg-gray-700 px-2 py-1 rounded transition duration-300"
                   >
                     Learning Roadmap
-                  </a>
+                  </Link>
                 </li>
                 {/* Add more submenu items as needed */}
               </ul>
             )}
           </li>
           <li>
-            <a
-              href="#"
+            <div
               onClick={toggleSecondSubMenu}
-              className="flex items-center justify-between hover:bg-gray-700 px-4 py-2 rounded transition duration-300"
+              className="flex items-center justify-between hover:bg-gray-700 px-4 py-2 rounded transition duration-300 cursor-pointer"
             >
               <span>Assessments</span>
               <span className="ml-2">{showSecondSubMenu ? "▲" : "▼"}</span>
-            </a>
+            </div>
             {showSecondSubMenu && (
               <ul className="pl-6">
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="#"
                     className="block hover:bg-gray-700 px-2 py-1 rounded transition duration-300"
                   >
                     Formative
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="#"
                     className="block hover:bg-gray-700 px-2 py-1 rounded transition duration-300"
                   >
                     Summative
-                  </a>
+                  </Link>
                 </li>
-
                 {/* Add more submenu items as needed */}
               </ul>
             )}
