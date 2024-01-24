0e038a1110490cb2768c99d2ae8c11ebf8622fcb
Girija Shankar <girijashankar.s@royalcyber.com>
2024-01-24 16:20:44 +0530

    select grade filter drop-down opration done for unitLession page

--- src/screens/Units/UnitLessions/UnitLessions.js
+++ src/screens/Units/UnitLessions/UnitLessions.js
@@ -4,6 +4,7 @@ const UnitLessions = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isUpdateMode, setIsUpdateMode] = useState(false);
   const [updateItemId, setUpdateItemId] = useState(null);
+  const [selectedGradeName, setSelectedGradeName] = useState(null);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -20,6 +21,7 @@ const UnitLessions = () => {
     {
       id: 0,
       unitName: "Sample Unit 1",
+      gradeName: "Grade 1",
       lessonName: "Lesson 1",
       duration: "2:30",
       assessment: "Assessment 1",
@@ -27,6 +29,7 @@ const UnitLessions = () => {
     {
       id: 1,
       unitName: "Sample Unit 2",
+      gradeName: "Grade 2",
       lessonName: "Lesson 2",
       duration: "3:00",
       assessment: "Assessment 2",
@@ -34,6 +37,7 @@ const UnitLessions = () => {
     {
       id: 2,
       unitName: "Sample Unit 3",
+      gradeName: "Grade 3",
       lessonName: "Lesson 3",
       duration: "1:45",
       assessment: "Assessment 3",
@@ -41,6 +45,7 @@ const UnitLessions = () => {
     {
       id: 3,
       unitName: "Sample Unit 4",
+      gradeName: "Grade 4",
       lessonName: "Lesson 4",
       duration: "2:15",
       assessment: "Assessment 4",
@@ -48,6 +53,7 @@ const UnitLessions = () => {
     {
       id: 4,
       unitName: "Sample Unit 5",
+      gradeName: "Grade 5",
       lessonName: "Lesson 5",
       duration: "1:30",
       assessment: "Assessment 5",
@@ -55,6 +61,7 @@ const UnitLessions = () => {
     {
       id: 5,
       unitName: "Sample Unit 6",
+      gradeName: "Grade 6",
       lessonName: "Lesson 6",
       duration: "2:00",
       assessment: "Assessment 6",
@@ -62,6 +69,7 @@ const UnitLessions = () => {
     {
       id: 6,
       unitName: "Sample Unit 7",
+      gradeName: "Grade 7",
       lessonName: "Lesson 7",
       duration: "1:15",
       assessment: "Assessment 7",
@@ -69,6 +77,7 @@ const UnitLessions = () => {
     {
       id: 7,
       unitName: "Sample Unit 8",
+      gradeName: "Grade 8",
       lessonName: "Lesson 8",
       duration: "2:45",
       assessment: "Assessment 8",
@@ -76,6 +85,7 @@ const UnitLessions = () => {
     {
       id: 8,
       unitName: "Sample Unit 9",
+      gradeName: "Grade 9",
       lessonName: "Lesson 9",
       duration: "1:00",
       assessment: "Assessment 9",
@@ -83,6 +93,7 @@ const UnitLessions = () => {
     {
       id: 9,
       unitName: "Sample Unit 10",
+      gradeName: "Grade 10",
       lessonName: "Lesson 10",
       duration: "2:30",
       assessment: "Assessment 10",
@@ -101,6 +112,7 @@ const UnitLessions = () => {
     const newUnit = {
       id: nextId++,
       unitName,
+      gradeName,
       lessonName,
       duration,
       assessment,
@@ -110,6 +122,7 @@ const UnitLessions = () => {
 
     // Reset the form data
     setUnitName('');
+    setGradeName('');
     setLessonName('');
     setDuration('');
     setAssessment('');
@@ -125,6 +138,7 @@ const UnitLessions = () => {
   
     if (selectedItem) {
       setUnitName(selectedItem.unitName);
+      setGradeName(selectedItem.gradeName);
       setLessonName(selectedItem.lessonName);
       setDuration(selectedItem.duration);
       setAssessment(selectedItem.assessment);
@@ -141,6 +155,7 @@ const UnitLessions = () => {
       item.id === updateItemId
         ? {
             ...item,
+            gradeName,
             unitName,
             lessonName,
             duration,
@@ -153,12 +168,14 @@ const UnitLessions = () => {
     closeModal();
     // Reset form fields
     setUnitName('');
+    setGradeName('');
     setLessonName('');
     setDuration('');
     setAssessment('');
   };
 
   const [unitName, setUnitName] = useState('');
+  const [gradeName, setGradeName] = useState('');
   const [lessonName, setLessonName] = useState('');
   const [duration, setDuration] = useState('');
   const [assessment, setAssessment] = useState('');
@@ -167,8 +184,12 @@ const UnitLessions = () => {
     console.log(itemId);
     setData((prevItems) => prevItems.filter((item) => item.id !== itemId));
   };
-
-  
+  const getUniqueGradeNames = () => {
+    const uniqueGradeNames = [...new Set(data.map(item => item.gradeName))];
+    return uniqueGradeNames;
+  };
+  const uniqueGradeNames = getUniqueGradeNames();
+  const filteredData = selectedGradeName ? data.filter((item) => item.gradeName === selectedGradeName) : data;
 
   
 
@@ -182,9 +203,13 @@ const UnitLessions = () => {
               <label className="block text-sm font-medium text-gray-600">
                 Select Grade
               </label>
-              <select className="mt-1 p-2 border rounded w-full max-w-64">
-                <option defaultValue>Select</option>
-                {/* Add more options as needed */}
+              <select className="mt-1 p-2 border rounded w-full max-w-64"
+              value={selectedGradeName}
+              onChange={(e) => setSelectedGradeName(e.target.value)}>
+                <option value="" defaultValue>Select</option>
+                {uniqueGradeNames.map((gradeName, index) => (
+                  <option key={index}>{gradeName}</option>
+                ))}
               </select>
             </div>
             <div>
@@ -201,6 +226,7 @@ const UnitLessions = () => {
             <thead>
               <tr className="bg-gray-200">
                 <th className="border p-2">Unit Name</th>
+                <th className="border p-2">Grade Name</th>
                 <th className="border p-2">Lesson Name</th>
                 <th className="border p-2">Duration (HH:MM)</th>
                 <th className="border p-2">Assessments</th>
@@ -208,20 +234,21 @@ const UnitLessions = () => {
               </tr>
             </thead>
             <tbody>
-              {data.map((data, index) => (
-                <tr key={index}>
+              {filteredData.map((data, index) => (
+                <tr key={data.id}>
                   <td className="border p-2">{data.unitName}</td>
+                  <td className="border p-2">{data.gradeName}</td>
                   <td className="border p-2">{data.lessonName}</td>
                   <td className="border p-2">{data.duration}</td>
                   <td className="border p-2">{data.assessment}</td>
                   <td className="border p-2">
                     <span className="text-blue-500 cursor-pointer mr-2"
-                    onClick={() => openUpdateModal(index)}>
+                    onClick={() => openUpdateModal(data.id)}>
                       Edit
                     </span>
                     <span className="text-gray-400">|</span>
                     <span className="text-red-500 cursor-pointer ml-2"
-                    onClick={() => handleDelete(index)}>
+                    onClick={() => handleDelete(data.id)}>
                       Delete
                     </span>
                   </td>
@@ -249,6 +276,18 @@ const UnitLessions = () => {
                   placeholder="Enter unit name"
                   value={unitName}
                   onChange={(e) => setUnitName(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Grade Name
+                </label>
+                <input
+                  type="text"
+                  className="mt-1 p-2 border rounded w-full"
+                  placeholder="Enter grade name"
+                  value={gradeName}
+                  onChange={(e) => setGradeName(e.target.value)}
                 />
               </div>
               <div className="mb-4">
